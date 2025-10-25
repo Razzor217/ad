370a4e3d6345472d9ac25b51359aff3d30e84bcd
Max Beddies <max.beddies@t-online.de>
2025-10-25 16:11:43 +0200

    add jacobian for functions f(x, u, t) wrt x

--- include/ad/detail/differentiation/eigen.h
+++ include/ad/detail/differentiation/eigen.h
@@ -5,9 +5,18 @@
 
 namespace ad::detail {
 
-template <typename F, typename T, int R, int C> struct eigen_function_traits {
+template <typename F, typename T, int R, int C> struct eigen_function_traits_nd {
     using argument_type = Eigen::Matrix<T, R, C>;
     using return_type = decltype(std::declval<F>()(std::declval<argument_type>()));
+
+    constexpr static int out_rows = return_type::RowsAtCompileTime;
+    constexpr static int out_cols = return_type::ColsAtCompileTime;
+};
+
+template <typename F, typename T, int X, int U> struct eigen_function_traits_xu {
+    using x_type = Eigen::Matrix<T, X, 1>;
+    using u_type = Eigen::Matrix<T, U, 1>;
+    using return_type = decltype(std::declval<F>()(std::declval<x_type>(), std::declval<u_type>(), std::declval<T>()));
 
     constexpr static int out_rows = return_type::RowsAtCompileTime;
     constexpr static int out_cols = return_type::ColsAtCompileTime;
@@ -23,10 +32,18 @@ concept eigen_function = requires(F f, Eigen::Matrix<T, R, C> X) {
 
 template <typename F, typename T, int R, int C>
 concept eigen_function_nd = R != Eigen::Dynamic && C != Eigen::Dynamic && requires(F f, Eigen::Matrix<T, R, C> X) {
-    requires detail::eigen_function_traits<F, T, R, C>::out_rows != Eigen::Dynamic;
-    requires detail::eigen_function_traits<F, T, R, C>::out_cols != Eigen::Dynamic;
+    requires detail::eigen_function_traits_nd<F, T, R, C>::out_rows != Eigen::Dynamic;
+    requires detail::eigen_function_traits_nd<F, T, R, C>::out_cols != Eigen::Dynamic;
     requires std::is_same_v<typename decltype(f(X))::Scalar, T>;
 };
+
+template <typename F, typename T, int X, int U>
+concept eigen_function_xu = X != Eigen::Dynamic && U != Eigen::Dynamic
+    && requires(F f, Eigen::Matrix<T, X, 1> x, Eigen::Matrix<T, U, 1> u, T dt) {
+           requires detail::eigen_function_traits_xu<F, T, X, U>::out_rows != Eigen::Dynamic;
+           requires detail::eigen_function_traits_xu<F, T, X, U>::out_cols != Eigen::Dynamic;
+           requires std::is_same_v<typename decltype(f(x, u, dt))::Scalar, T>;
+       };
 
 template <typename F, typename T, int M>
     requires eigen_function<F, T, M, 1>
@@ -54,7 +71,7 @@ template <typename F, typename T, int M>
     requires eigen_function_nd<F, T, M, 1>
 auto jacobian(F f, Eigen::Matrix<T, M, 1> const& X)
 {
-    constexpr int N = detail::eigen_function_traits<F, T, M, 1>::out_rows;
+    constexpr int N = detail::eigen_function_traits_nd<F, T, M, 1>::out_rows;
     Eigen::Matrix<T, N, M> J { Eigen::Matrix<T, N, M>::Zero() };
 
     for (int m {}; m < M; ++m) {
@@ -71,4 +88,30 @@ auto jacobian(F f, Eigen::Matrix<T, M, 1> const& X)
     return J;
 }
 
+template <typename F, typename T, int X, int U>
+    requires eigen_function_xu<F, T, X, U>
+auto jacobian(F f, Eigen::Matrix<T, X, 1> const& x, Eigen::Matrix<T, U, 1> const& u, T const dt)
+{
+    constexpr int N = detail::eigen_function_traits_xu<F, T, X, U>::out_rows;
+    Eigen::Matrix<T, N, X> J { Eigen::Matrix<T, N, X>::Zero() };
+
+    Eigen::Matrix<Dual<T>, U, 1> du { Eigen::Matrix<Dual<T>, U, 1>::Constant(Dual<T> {}) };
+    for (int i {}; i < U; ++i)
+        du(i) = { u(i) };
+
+    Dual<T> const ddt { dt };
+
+    for (int ix {}; ix < X; ++ix) {
+        Eigen::Matrix<Dual<T>, X, 1> dx { Eigen::Matrix<Dual<T>, X, 1>::Constant(Dual<T> {}) };
+        for (int i {}; i < X; ++i)
+            dx(i) = { x(i), (i == ix) ? T { 1. } : T { 0. } };
+
+        auto df = f(dx, du, ddt);
+
+        for (int n {}; n < N; ++n)
+            J(n, ix) = df(n).dual;
+    }
+
+    return J;
+}
 }
--- src/examples/demo_eigen.cpp
+++ src/examples/demo_eigen.cpp
@@ -12,5 +12,16 @@ auto main() -> int
 
     std::cout << ad::jacobian(f, Eigen::Vector3d { 1., 2., 3. }) << std::endl;
 
+    auto g = []<typename T>(Eigen::Matrix<T, 6, 1> const& x, Eigen::Matrix<T, 3, 1> const& u, T const dt) {
+        Eigen::Matrix<T, 6, 1> f { x };
+        f.template segment<3>(3) += dt * u;
+        f.template segment<3>(0) += dt * f.template segment<3>(3);
+        return f;
+    };
+
+    std::cout << ad::jacobian(
+        g, Eigen::Matrix<double, 6, 1> { Eigen::Matrix<double, 6, 1>::Zero() }, Eigen::Vector3d { 2., 2., 2. }, .01)
+              << std::endl;
+
     return 0;
 }
