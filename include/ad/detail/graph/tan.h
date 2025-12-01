54771082236839b5b56b2ac93a3b111c83e81e78
Max Beddies <max.beddies@t-online.de>
2025-12-01 15:46:22 +0100

    add tangens node implementation

---
+++ include/ad/detail/graph/tan.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <ad/detail/graph/constant.h>
+#include <ad/detail/graph/node.h>
+
+#include <cmath>
+
+namespace ad {
+
+template <node A> class tan_impl {
+public:
+    using argument_type = A;
+    using argument_reference = argument_type&;
+    using value_type = typename argument_type::value_type;
+
+    auto value() const -> value_type { return m_value; }
+
+    void gradient(value_type const adjoint)
+    {
+        m_arg.gradient(adjoint / (std::cos(m_arg.value()) * std::cos(m_arg.value())));
+    }
+
+    explicit tan_impl(argument_reference& arg)
+        : m_arg { arg }
+        , m_value { std::tan(m_arg.value()) }
+    {
+    }
+
+private:
+    argument_reference m_arg {};
+    value_type m_value {};
+};
+
+template <node A> auto tan(A& arg) -> tan_impl<A> { return tan_impl { arg }; }
+template <typename T> auto tan(T const arg) -> constant<T> { return constant { std::tan(arg) }; }
+
+}
